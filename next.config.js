5211e0015471d466b3a52815cf8a95d9ef22f83c
MihiroH <mihiro.hashimoto.1125@gmail.com>
2021-08-14 02:46:11 +0900

    Add: config files and change directory instructure

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
-module.exports = {
-  reactStrictMode: true,
+const path = require('path');
+
+module.exports = (phase, { defaultConfig }) => {
+  defaultConfig['reactStrictMode'] = true;
+
+  if ("sassOptions" in defaultConfig) {
+    defaultConfig["sassOptions"] = {
+      includePaths: [path.join(__dirname, 'styles')],
+      prependData: `@import "config.sass"`,
+    };
+  }
+
+  return defaultConfig;
 }
