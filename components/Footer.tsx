50d2f87714438684cf4076ffe8c4a47fee221be7
Yuto <youto_drum@yahoo.co.jp>
2021-08-26 02:57:59 +0900

    Add: common Footer component, Footer style file (Sass file)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,41 @@
-const Footer: React.VFC = (): JSX.Element => {
-  return <footer></footer>
+import Link from 'next/link'
+import styles from '@/styles/Footer.module.sass'
+
+const Footer: React.VFC = () => {
+  return (
+    <footer className={styles['footer']}>
+      <ul>
+        <li className={styles['item']}>
+          <Link href="/">
+            <a>プライバシーポリシー</a>
+          </Link>
+        </li>
+        <li className={styles['item']}>
+          <Link href="/">
+            <a>特定商取引法に基づく表記</a>
+          </Link>
+        </li>
+        <li className={styles['item']}>
+          <Link href="/">
+            <a>返金ポリシー</a>
+          </Link>
+        </li>
+        <li className={styles['item']}>
+          <Link href="/">
+            <a>お問い合わせ</a>
+          </Link>
+        </li>
+        <li className={styles['item']}>
+          <Link href="/">
+            <a>解約手続き</a>
+          </Link>
+        </li>
+      </ul>
+      <small className={styles['copyright']}>
+        © 2021 a-programming All Right Reserved.
+      </small>
+    </footer>
+  )
 }
 
 export default Footer
