d3ec23473c513731ed6451c8c4c2c34f7c9484da
Yuto <youto_drum@yahoo.co.jp>
2021-08-27 02:10:07 +0900

    Fix: Footer components, footer style file(sass file)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,38 +1,75 @@
 import Link from 'next/link'
+import BaseText from '@/components/BaseText'
 import styles from '@/styles/Footer.module.sass'
+
+const contentsArray = [
+  {
+    name: 'プライバシーポリシー',
+    href: '/',
+  },
+  {
+    name: '特定商取引法に基づく表記',
+    href: '/',
+  },
+  {
+    name: '返金ポリシー',
+    href: '/',
+  },
+  {
+    name: 'お問い合わせ',
+    href: 'mailto:apro.yuto.engineer@gmail.com',
+  },
+  {
+    name: '解約手続き',
+    href: 'mailto:apro.yuto.engineer@gmail.com',
+  },
+]
+
+const content = contentsArray.map((item, i) => (
+  <li key={i} className={styles.item}>
+    <Link href={item.href}>
+      <a>
+        <BaseText sizePc={14} sizeSp={20}>
+          {item.name}
+        </BaseText>
+      </a>
+    </Link>
+  </li>
+))
 
 const Footer: React.VFC = () => {
   return (
-    <footer className={styles['footer']}>
+    <footer className={styles.footer}>
       <ul>
-        <li className={styles['item']}>
+        {content}
+        {/* <li className={styles.item}>
           <Link href="/">
             <a>プライバシーポリシー</a>
           </Link>
         </li>
-        <li className={styles['item']}>
+        <li className={styles.item}>
           <Link href="/">
             <a>特定商取引法に基づく表記</a>
           </Link>
         </li>
-        <li className={styles['item']}>
+        <li className={styles.item}>
           <Link href="/">
             <a>返金ポリシー</a>
           </Link>
         </li>
-        <li className={styles['item']}>
+        <li className={styles.item}>
           <Link href="/">
             <a>お問い合わせ</a>
           </Link>
         </li>
-        <li className={styles['item']}>
+        <li className={styles.item}>
           <Link href="/">
             <a>解約手続き</a>
           </Link>
-        </li>
+        </li> */}
       </ul>
-      <small className={styles['copyright']}>
-        © 2021 a-programming All Right Reserved.
+      <small className={styles.copyright}>
+        &#x000A9; 2021 a-programming All Right Reserved.
       </small>
     </footer>
   )
