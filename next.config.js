279a580797054b7ad3d02e38f371da9926f2f4c8
MihiroH <mihiro.hashimoto.1125@gmail.com>
2021-08-31 20:42:16 +0900

    Fix: uninstall svg-loader(babel-plugin-inline-react-svg) and install @svgr/webpack
    Memo: because babel-plugin-inline-react-svg delete viewbox of svg property

--- next.config.js
+++ next.config.js
@@ -1,13 +1,28 @@
 const path = require('path')
 
-module.exports = (phase, { defaultConfig }) => {
+module.exports = (_, { defaultConfig }) => {
   defaultConfig['reactStrictMode'] = true
 
   if ('sassOptions' in defaultConfig) {
     defaultConfig['sassOptions'] = {
       includePaths: [path.join(__dirname, 'styles')],
-      prependData: `@import "config.sass"`,
+      prependData: '@import "config.sass"',
     }
+  }
+
+  defaultConfig['webpack'] = (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: '@svgr/webpack',
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [{ removeViewBox: false }],
+        },
+      },
+    })
+    return config
   }
 
   return defaultConfig
