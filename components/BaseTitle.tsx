351e1c9692eb360429cb2780db46597cafbbbaa3
MihiroH <mihiro.hashimoto.1125@gmail.com>
2021-09-11 19:53:22 +0900

    Refactor: type of props's level of BaseTitle

--- components/BaseTitle.tsx
+++ components/BaseTitle.tsx
@@ -2,7 +2,7 @@ import type { Props as BaseProps } from '@/types/BaseText'
 import BaseText from '@/components/BaseText'
 
 type Props = BaseProps & {
-  level: '1' | '2' | '3' | '4' | '5' | '6'
+  level: 1 | 2 | 3 | 4 | 5 | 6
 }
 
 const BaseTitle: React.VFC<Props> = ({ level, children, ...props }) => {
