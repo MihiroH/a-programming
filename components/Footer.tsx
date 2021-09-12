93f4d4d82763878956858ea8c5670b96f8c120f1
MihiroH <mihiro.hashimoto.1125@gmail.com>
2021-09-12 17:06:10 +0900

    Fix: Footer navigation

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import BaseText from '@/components/BaseText'
 import styles from '@/styles/Footer.module.sass'
 
@@ -6,41 +7,55 @@ const menuList = [
   {
     name: 'プライバシーポリシー',
     href: '/',
+    external: false,
   },
   {
     name: '特定商取引法に基づく表記',
     href: '/',
-  },
-  {
-    name: '返金ポリシー',
-    href: '/',
+    external: false,
   },
   {
     name: 'お問い合わせ',
     href: 'mailto:apro.yuto.engineer@gmail.com',
-  },
-  {
-    name: '解約手続き',
-    href: 'mailto:apro.yuto.engineer@gmail.com',
+    external: true,
   },
 ]
 
-const listEl = menuList.map((item, i) => (
-  <li key={i} className={styles.item}>
-    <Link href={item.href}>
-      <a>
+type Props = {
+  className?: string
+}
+
+const Footer: React.VFC<Props> = ({ className }) => {
+  const router = useRouter()
+
+  const goTo = ({ href = '', external = false } = {}) => {
+    if (external) {
+      window.open(href, '_blank')
+      return
+    }
+
+    router.push(href)
+  }
+
+  const listEls = menuList.map((item, i) => (
+    <li key={i} className={styles.item}>
+      <span
+        role="link"
+        className={styles.anchor}
+        onClick={() => goTo({ href: item.href, external: item.external })}
+      >
         <BaseText sizeSp={20} sizePc={14}>
           {item.name}
         </BaseText>
-      </a>
-    </Link>
-  </li>
-))
+      </span>
+    </li>
+  ))
 
-const Footer: React.VFC = () => {
   return (
-    <footer className={styles.footer}>
-      <ul>{listEl}</ul>
+    <footer className={[styles.footer, className].join(' ')}>
+      <nav>
+        <ul>{listEls}</ul>
+      </nav>
       <BaseText
         tagName="small"
         className={styles.copyright}
